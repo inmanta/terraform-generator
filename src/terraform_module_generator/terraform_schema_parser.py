703e2d1919295da848c6725d4a993d8ca2517aca
Guillaume Everarts de Velp <daman@gamalle.org>
2022-05-28 17:07:51 +0200

    Repair schema parser

--- src/terraform_module_generator/terraform_schema_parser.py
+++ src/terraform_module_generator/terraform_schema_parser.py
@@ -3,17 +3,12 @@
     :contact: code@inmanta.com
     :license: Inmanta EULA
 """
-import string
 from textwrap import dedent, indent
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, List, Literal, Tuple
 
 from inmanta_module_factory.builder import InmantaModuleBuilder
-from inmanta_module_factory.helpers.const import INMANTA_RESERVED_KEYWORDS
-from inmanta_module_factory.inmanta.attribute import (
-    Attribute,
-    InmantaAttributeType,
-    InmantaPrimitiveList,
-)
+from inmanta_module_factory.helpers.utils import inmanta_entity_name, inmanta_safe_name
+from inmanta_module_factory.inmanta.attribute import Attribute
 from inmanta_module_factory.inmanta.entity import Entity
 from inmanta_module_factory.inmanta.entity_relation import EntityRelation
 from inmanta_module_factory.inmanta.implement import Implement
@@ -21,23 +16,15 @@ from inmanta_module_factory.inmanta.implementation import Implementation
 from inmanta_module_factory.inmanta.index import Index
 from inmanta_module_factory.inmanta.module_element import DummyModuleElement
 from inmanta_module_factory.inmanta.plugin import Plugin, PluginArgument
-
-
-def snake_to_pascal(name: str) -> str:
-    parts = name.split("_")
-    return "".join([part[0].upper() + part[1:] for part in parts])
-
-
-def make_inmanta_safe(input: str) -> str:
-    input = input.lower()
-
-    if input in INMANTA_RESERVED_KEYWORDS:
-        return input + "_x"
-
-    if input[0] in string.digits:
-        return "x_" + input
-
-    return input
+from inmanta_module_factory.inmanta.types import (
+    InmantaAnyType,
+    InmantaBooleanType,
+    InmantaDictType,
+    InmantaListType,
+    InmantaPrimitiveType,
+    InmantaStringType,
+    InmantaType,
+)
 
 
 def config_plugin_name(entity: Entity) -> str:
@@ -65,16 +52,16 @@ class TerraformSchemaParser:
         self.base_entity = Entity(
             name="Base",
             path=[self.module_name, "common"],
-            attributes=[
+            fields=[
                 Attribute(
                     name="inmanta_id",
-                    inmanta_type="string",
+                    inmanta_type=InmantaStringType,
                     optional=False,
                     description="This is the identifier of the entity.",
                 ),
                 Attribute(
                     name="terraform_id",
-                    inmanta_type="string",
+                    inmanta_type=InmantaStringType,
                     optional=True,
                     default="null",
                     description="This is the identifier of the terraform resource to import.",
@@ -90,10 +77,10 @@ class TerraformSchemaParser:
         resource_base_entity = Entity(
             name="Resource",
             path=[self.module_name],
-            attributes=[
+            fields=[
                 Attribute(
                     name="purged",
-                    inmanta_type="bool",
+                    inmanta_type=InmantaBooleanType,
                     optional=False,
                     default="false",
                     description="Set this to true to purge the related terraform resource.",
@@ -140,12 +127,12 @@ class TerraformSchemaParser:
             name="provider",
             path=resource_base_entity.path,
             entity=resource_base_entity,
-            arity=(1, 1),
+            cardinality=(1, 1),
             description="A relation to the resource provider.",
             peer=EntityRelation(
                 name="resources",
                 path=provider.path,
-                arity=(0, None),
+                cardinality=(0, None),
                 entity=provider,
             ),
         )
@@ -157,7 +144,7 @@ class TerraformSchemaParser:
             resource_path_filtered = []
             last_resource_path_elem = ""
             for resource_path_elem in resource_path:
-                resource_path_elem = make_inmanta_safe(resource_path_elem)
+                resource_path_elem = inmanta_safe_name(resource_path_elem)
 
                 if resource_path_elem != last_resource_path_elem:
                     resource_path_filtered.append(resource_path_elem)
@@ -190,16 +177,16 @@ class TerraformSchemaParser:
             name="terraform_resource",
             path=resource_entity.path,
             entity=resource_entity,
-            arity=(1, 1),
+            cardinality=(1, 1),
             description="A relation to the resource from the terraform module.",
             peer=EntityRelation(
                 name="",
                 path=["terraform"],
-                arity=(0, 0),
+                cardinality=(0, 0),
                 entity=Entity(
                     name="Resource",
                     path=["terraform"],
-                    attributes=[],
+                    fields=[],
                 ),
             ),
         )
@@ -211,16 +198,16 @@ class TerraformSchemaParser:
             name="provider",
             path=provider_entity.path,
             entity=provider_entity,
-            arity=(1, 1),
+            cardinality=(1, 1),
             description="A relation to the provider from the terraform module.",
             peer=EntityRelation(
                 name="",
-                arity=(0, 0),
+                cardinality=(0, 0),
                 path=["terraform"],
                 entity=Entity(
                     name="Provider",
                     path=["terraform"],
-                    attributes=[],
+                    fields=[],
                 ),
             ),
         )
@@ -253,7 +240,7 @@ class TerraformSchemaParser:
     ) -> Implementation:
         implementation = Implementation(
             name="config",
-            path=resource_entity.path + [make_inmanta_safe(resource_raw_name)],
+            path=resource_entity.path + [inmanta_safe_name(resource_raw_name)],
             entity=resource_entity,
             content=dedent(
                 f"""
@@ -296,35 +283,35 @@ class TerraformSchemaParser:
             attributes_mapping.setdefault(attribute.name, entity_attribute)
 
         entity = Entity(
-            name=snake_to_pascal(raw_name),
+            name=inmanta_entity_name(raw_name),
             path=path,
-            attributes=attributes,
+            fields=attributes,
             parents=[self.base_entity] if has_id else [],
         )
         self.module_builder.add_module_element(entity)
 
         for block_type in schema_block.block_types:
             embedded_entity = self.parse_entity(
-                path=path + [make_inmanta_safe(raw_name)],
-                raw_name=make_inmanta_safe(block_type.type_name),
+                path=path + [inmanta_safe_name(raw_name)],
+                raw_name=inmanta_safe_name(block_type.type_name),
                 schema_block=block_type.block,
                 has_id=has_id,
             )
 
-            max_arity = block_type.max_items
-            if max_arity == 0:
-                max_arity = None
+            max_cardinality = block_type.max_items
+            if max_cardinality == 0:
+                max_cardinality = None
 
             relation = EntityRelation(
-                name=make_inmanta_safe(block_type.type_name),
+                name=inmanta_safe_name(block_type.type_name),
                 path=path,
                 entity=entity,
-                arity=(block_type.min_items, max_arity),
+                cardinality=(block_type.min_items, max_cardinality),
                 peer=EntityRelation(
                     name="",
                     path=path,
                     entity=embedded_entity,
-                    arity=(0, 0),
+                    cardinality=(0, 0),
                 ),
             )
             self.module_builder.add_module_element(relation)
@@ -339,7 +326,7 @@ class TerraformSchemaParser:
             index = Index(
                 path=entity.path,
                 entity=entity,
-                attributes=self.base_entity.attributes[:1],
+                fields=self.base_entity.attributes[:1],
             )
             self.module_builder.add_module_element(index)
 
@@ -392,7 +379,7 @@ class TerraformSchemaParser:
                         "\n"
                     )
                 ).strip()
-                if relation.arity_max == "1":
+                if relation.cardinality_max == "1":
                     value = f"[{plugin_name}(entity.{relation.name})]"
 
             if nesting == 4:
@@ -407,7 +394,7 @@ class TerraformSchemaParser:
                         "\n"
                     )
                 ).strip()
-                if relation.arity_max == "1":
+                if relation.cardinality_max == "1":
                     value = dedent(
                         f"""
                         {{
@@ -449,21 +436,24 @@ class TerraformSchemaParser:
     def parse_attribute(self, schema: Any) -> Attribute:
         type = schema.type.decode("utf-8")
         return Attribute(
-            name=make_inmanta_safe(schema.name),
+            name=inmanta_safe_name(schema.name),
             inmanta_type=self.parse_type(type),
             optional=schema.optional,
             description=schema.description,
         )
 
-    def parse_type(self, raw_type: str) -> InmantaAttributeType:
-        def parse_primitive(input_type: str) -> str:
+    def parse_type(self, raw_type: str) -> InmantaType:
+        def parse_primitive(input_type: str) -> InmantaType:
             parsed = input_type.strip('"')
-            if parsed in ["string", "bool", "number", "any"]:
-                return parsed
+            if parsed in ["string", "bool", "number"]:
+                return InmantaPrimitiveType(parsed)
+
+            if parsed == "any":
+                return InmantaAnyType
 
             raise Exception(f"Unknown primitive type: {input_type}")
 
-        def parse_aggregate(input_type: str) -> str:
+        def parse_aggregate(input_type: str) -> Literal["list", "dict"]:
             parsed = input_type.strip('"')
             if parsed in ["list", "set"]:
                 return "list"
@@ -483,7 +473,7 @@ class TerraformSchemaParser:
                 raise Exception(f"Unknown aggregate type: {raw_type}")
 
             aggregate = ""
-            primitive = ""
+            primitive = InmantaAnyType
             if parsed[0].startswith('"'):
                 aggregate = parse_aggregate(parsed[0])
 
@@ -491,7 +481,7 @@ class TerraformSchemaParser:
                 primitive = parse_primitive(parsed[1])
 
             if aggregate == "dict":
-                return "dict"
-            return InmantaPrimitiveList(primitive)
+                return InmantaDictType
+            return InmantaListType(primitive)
 
         raise Exception(f"Unknown type: '{raw_type}'")
